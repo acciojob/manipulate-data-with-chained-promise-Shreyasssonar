f2fc1d64fcbff7e466cb02f5e5e6a7a3193f3674
Shreyas <121279572+Shreyasssonar@users.noreply.github.com>
2024-01-18 07:21:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
-//your JS code here. If required.
+/*  your css code here. If applicable */
+function manipulateData(inputArray) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(inputArray);
+    }, 3000);
+  })
+  .then((data) => {
+    // Filter out odd numbers after 1 second
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        const filteredArray = data.filter(num => num % 2 === 0);
+        document.getElementById('output').innerText = filteredArray.toString();
+        resolve(filteredArray);
+      }, 1000);
+    });
+  })
+  .then((filteredArray) => {
+    // Multiply even numbers by 2 after 2 seconds
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        const multipliedArray = filteredArray.map(num => num * 2);
+        document.getElementById('output').innerText = multipliedArray.toString();
+        resolve(multipliedArray);
+      }, 2000);
+    });
+  });
+}
+
+const inputArray = [1, 2, 3, 4];
+
+manipulateData(inputArray);
